750dc9e930d537d6078779d8ee30d549f537d44a
Andrew Mullan <admullan@gmail.com>
2021-04-13 10:55:34 -0700

    forgot return 0 on challenge solution

--- Section4/ChallengeSolution/main.cpp
+++ Section4/ChallengeSolution/main.cpp
@@ -7,4 +7,6 @@ int main() {
     std::cin >> favorite_number;
     std::cout << "Amazing!! That's my favorite number too!" << std::endl;
     std::cout << "No really!!, " << favorite_number << " is my favorite number!" << std::endl;
+    
+    return 0;
 }
